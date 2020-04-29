33fa1f5c566928b1a1ef55eeb9c00aebd216b2ec
JHOSEP GRANDA <jhosep.granda@educatic.com.co>
2020-04-29 09:16:48 -0500

    cambio de pagina a bucket de amazon

--- src/main/java/co/com/devco/userinterfaces/PetBookHomePage.java
+++ src/main/java/co/com/devco/userinterfaces/PetBookHomePage.java
@@ -4,7 +4,7 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class PetBookHomePage {
-	public static final String PETBOOK_HOME = "localhost:4200";
+	public static final String PETBOOK_HOME = "http://pet-book-project.s3-website-us-west-1.amazonaws.com";
 	public static final Target BUTTON_PERROS = Target.the("Boton de filtro de perros").located(By.id("filterPerros"));
 	public static final Target BUTTON_GATOS = Target.the("Boton de filtro de gatos").located(By.id("filterGatos"));
 	public static final Target IMAGENES_VISIBLES = Target.the("Lista de imagenes visibles").located(By.xpath("//*[@id='listaImagenes']/li//img"));
